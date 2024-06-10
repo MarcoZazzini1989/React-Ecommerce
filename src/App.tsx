14520c221e1ad36c8b2c05b0c0193aa5e8331319
Marco Zazzini <47858428+MarcoZazzini1989@users.noreply.github.com>
2024-06-10 15:38:02 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navigation from "./Navigation/Nav";
+
+function App() {
+  return (
+    <>
+      <p>Ciao</p>
+      <p>miao</p>
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
