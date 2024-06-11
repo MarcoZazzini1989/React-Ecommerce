320dcee26c279b9c11e8f816a0564e90b6b3d739
Marco Zazzini <47858428+MarcoZazzini1989@users.noreply.github.com>
2024-06-11 10:53:13 +0000

    product tsx

--- src/Products/Products.tsx
+++ src/Products/Products.tsx
@@ -1,7 +1,38 @@
 import "./Products.css";
+import { IoMdStar } from "react-icons/io";
+import { IoBagAdd } from "react-icons/io5";
 
 function Products() {
-  return <div>Scarpe</div>;
+  return (
+    <>
+      <section className="card-conteiner">
+        <section className="card">
+          <img
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="Shoe"
+          />
+          <div className="card-details">
+            <h3 className="card-title">Shoes</h3>
+            <section className="card-reviews">
+              <IoMdStar />
+              <IoMdStar />
+              <IoMdStar />
+              <IoMdStar />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>$300</del>$200
+              </div>
+              <div className="bag">
+                <IoBagAdd />
+              </div>
+            </section>
+          </div>
+        </section>
+      </section>
+    </>
+  );
 }
 
 export default Products;
