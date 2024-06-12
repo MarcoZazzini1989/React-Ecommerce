4877a96a3ec664c5f0753d9d20813c259459e60a
Marco Zazzini <47858428+MarcoZazzini1989@users.noreply.github.com>
2024-06-12 02:03:36 +0000

    add Dynamic

--- src/Sidebar/Category/Category.tsx
+++ src/Sidebar/Category/Category.tsx
@@ -1,30 +1,12 @@
 import "./Category.css";
+import Input from "../../components/Input";
 
 function Category() {
   return (
     <div>
       <h2 className="sidebar-title">Category</h2>
       <div>
-        <label className="sidebar-label-container">
-          <input type="radio" name="text" />
-          <span className="checkmark"></span>All
-        </label>
-        <label className="sidebar-label-container">
-          <input type="radio" name="text" />
-          <span className="checkmark"></span>Sneakers
-        </label>
-        <label className="sidebar-label-container">
-          <input type="radio" name="text" />
-          <span className="checkmark"></span>Flats
-        </label>
-        <label className="sidebar-label-container">
-          <input type="radio" name="text" />
-          <span className="checkmark"></span>Sandals
-        </label>
-        <label className="sidebar-label-container">
-          <input type="radio" name="text" />
-          <span className="checkmark"></span>Heels
-        </label>
+        <Input />
       </div>
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,37 @@
+import { IoMdStar } from "react-icons/io";
+import { IoBagAdd } from "react-icons/io5";
+
 function Card() {
-  return <div>Card</div>;
+  return (
+    <>
+      <section className="card">
+        <img
+          src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+          alt="Shoe"
+          className="card-img"
+        />
+        <div className="card-details">
+          <h3 className="card-title">Shoes</h3>
+          <section className="card-reviews">
+            <IoMdStar className="rating-star" />
+            <IoMdStar className="rating-star" />
+            <IoMdStar className="rating-star" />
+            <IoMdStar className="rating-star" />
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del>$200
+            </div>
+            <div className="bag">
+              <IoBagAdd className="bag-icon" />
+            </div>
+          </section>
+        </div>
+      </section>
+      ;
+    </>
+  );
 }
 
 export default Card;
